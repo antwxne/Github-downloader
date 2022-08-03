ce567ec55b96efdf5d34555e9f3c1d0a5bf1ea0b
Antoine DESRUET <antoine.desruet@epitech.eu>
2022-08-03 02:58:24 +0200

    feat: new name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Github repo downloader",
+    name="gh-downloader",
     version="0.0.1",
     author="antwxne",
     author_email="antoine.desruet@epitech.eu",
